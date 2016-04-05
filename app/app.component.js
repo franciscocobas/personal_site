9b06604737eb3ddeead15008833e64ec0ded6edb
Francisco Cobas <fcarocena@gmail.com>
2016-04-04 23:12:21 -0300

    Two way data binding

--- app/app.component.js
+++ app/app.component.js
@@ -11,20 +11,31 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent;
+    var Hero, AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             }],
         execute: function() {
+            Hero = (function () {
+                function Hero() {
+                }
+                return Hero;
+            }());
+            exports_1("Hero", Hero);
             AppComponent = (function () {
                 function AppComponent() {
+                    this.title = 'Tour of heroes';
+                    this.hero = {
+                        id: 1,
+                        name: 'WindStorm'
+                    };
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My personal site</h1>'
+                        template: "\n      <h1>{{title}}</h1>\n      <h2>{{hero.name}} details!</h2>\n      <div><label>id: </label>{{hero.id}}</div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n      </div>"
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
